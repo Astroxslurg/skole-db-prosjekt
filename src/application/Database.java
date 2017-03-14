47b326d8e19a33e179b01ee72a1ef9ad2e8b53f9
christholest <christ.holest@outlook.com>
2017-03-14 12:39:21 +0100

    removed group from ovelse template

--- src/application/Database.java
+++ src/application/Database.java
@@ -60,17 +60,16 @@ public class Database {
     }
     		
     public static void insertGoal(){
-    	try{
+    	try {
     		Connection conn = DriverManager.getConnection(mysqlAddr, mysqlUser, mysqlPass);
     		PreparedStatement stmt = conn.prepareStatement("insert into KONDISJONSMAAL (Lengde, Tid, Type) values (?,?,?)");
-    		
     		stmt.executeUpdate();
-    	}catch(SQLException e) {
+    	} catch(SQLException e) {
         	System.out.println(e);
     	}
     }
     
-    public static void insertResult(String ovelse, int type, ) {
+    public static void insertResult(String ovelse, int type) {
     	try {
     		Connection conn = DriverManager.getConnection(mysqlAddr, mysqlUser, mysqlPass);
     		//If the exercise is a "kondisjon" exercise
